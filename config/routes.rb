b35c4426db272546f7a156b679f6f463ab0923be
Brandon Thompson <brandon.joseph.thompson@gmail.com>
2020-12-08 20:43:22 -0700

    Fast-forward the test creation videos

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   root 'pages#home'
-
   get 'about', to: 'pages#about'
   resources :articles
   get 'signup', to: 'users#new'
@@ -10,4 +7,5 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  resources :categories, except: [:destroy]
 end
